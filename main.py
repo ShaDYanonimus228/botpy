0a4fa1c9fdbe9d013f3758a7d8f89ebcd5d31394
ShaDYanonimus228 <97559805+ShaDYanonimus228@users.noreply.github.com>
2023-08-25 17:50:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#6589678614:AAEKwUHM-Z52EdphhU5Pq9OA6f7z5agQA4Y
+import telebot
+from datetime import datetime
+from pyrogram import*
+from api import*
+import time
+my_chat_id = 1524610051
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+bot = telebot.TeleBot('6589678614:AAEKwUHM-Z52EdphhU5Pq9OA6f7z5agQA4Y')
+
+api_id = 20570867
+api_hash = '0e123778ea491f4425b56216ae022434'
+
+with Client("my_account", api_id, api_hash) as app:
+    while True:
+        time.sleep(1)
+        print(current_time)
+        if current_time == current_time:  # Выставляете ваше время
+            print('pass')
+            app.send_message('@cryptoivests', "Разбуди меня,пожалуйста")
+            app.send_message('@cryptoivests', "Разбуди меня,пожалуйста")
+            app.send_message('@cryptoivests', "Разбуди меня,пожалуйста")
+
+
+
