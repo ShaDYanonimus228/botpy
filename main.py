b9fd25f6a52a63dadd1982439846991ca8dd105a
ShaDYanonimus228 <97559805+ShaDYanonimus228@users.noreply.github.com>
2023-08-28 00:30:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from pyrogram import*
 from api import*
 import time
 my_chat_id = 1524610051
-now = datetime.now()
-current_time = now.strftime("%H:%M")
 
 api_id = 20570867
 api_hash = '0e123778ea491f4425b56216ae022434'
 
 with Client("my_account", api_id, api_hash) as app:
     while True:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+
         time.sleep(1)
         print(current_time)
-        if current_time == '13:00':  # Выставляете ваше время
+        if current_time == '10:00':  # Выставляете ваше время
             print('pass')
             app.send_message('@banan123455', "Разбуди меня,пожалуйста")
             app.send_message('@banan123455', "Разбуди меня,пожалуйста")
